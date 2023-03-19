176952aad239608d52ae10ace492addc050a9c6c
YukioSenpai <steven.travers20@gmail.com>
2023-03-19 20:01:24 +0100

    tets

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+
 import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <StrictMode>
+          <App />
+  </StrictMode>,
+  document.getElementById('root')
 )
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
   plugins: [checker({ typescript: true }), react(), svgr(), tsconfigPaths()],
 })
